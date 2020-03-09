0c672ec2647da5290806d1053876b1732b4af55b
Johnboi <johndesmond631@gmail.com>
2020-03-09 07:39:24 -0400

    not able to connect to s3 when running python on EC2, saving configuration for reference

--- DataProcessing/PySpark/loan_processor.py
+++ DataProcessing/PySpark/loan_processor.py
@@ -27,12 +27,21 @@ class LoanProcessor(object):
 
     def start_pyspark(self, configs):
         sc = SparkContext()
+        hadoopConf = sc._jsc.hadoopConfiguration()
+        hadoopConf.set("fs.s3a.awsAccessKeyId", configs['accessKey'])
+        hadoopConf.set("fs.s3a.awsSecretAccessKey", configs['secretKey'])
+        hadoopConf.set("fs.s3a.endpoint", "https://lending-club-loan-pipeline.s3.amazonaws.com")
+        hadoopConf.set("com.amazonaws.services.s3a.enableV4", "true")
+        hadoopConf.set("fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem")
+
         spark = SparkSession(sc).builder.master(configs['master']).appName("LoanProcessor").getOrCreate()
         # %s order = access key | secret key | s3URL
         # DATA VALIDATION PLZ
-        loans = spark.read.csv("s3a://%s" % (configs['loanURL']))
-        loans.show()
+        # loans = spark.read.csv("s3a://%s" % (configs['loanURL']))
+        # loans.show()
+
         datadict = spark.read.csv("s3a://%s" % (configs['dictURLcsv']))
+        # datadict = spark.read.csv("s3a://%s:%s@%s" % (configs['accessKey'], configs['secretKey'], configs['dictURLcsv']))
         datadict.show()
 
         # loans = spark.read.csv("s3a://%s:%s@%s" % (configs['accessKey'], configs['secretKey'], configs['loanURL']))
